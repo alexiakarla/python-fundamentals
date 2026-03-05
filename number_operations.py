8b096fbfa4493984adc290e87a4fbf7b65ad77aa
Alexia Almeida <alexia.karla@gmail.com>
2026-03-04 22:51:05 -0300

    number_operations.py
    Add number operations exercise

---
+++ number_operations.py
@@ -0,0 +1,12 @@
+# This program calculates the double, triple and half of a number.
+
+number = 5
+
+double = number * 2
+triple = number * 3
+half = number / 2
+
+print(f"Number: {number}")
+print(f"Double: {double}")
+print(f"Triple: {triple}")
+print(f"Half: {half}")
