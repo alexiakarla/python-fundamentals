125b0896cbca14fcd56fb068c248892916a8f931
Alexia Almeida <alexia.karla@gmail.com>
2026-03-04 23:12:08 -0300

    Create bmi_calculator.py
    Add BMI calculator exercise

---
+++ bmi_calculator.py
@@ -0,0 +1,10 @@
+# This program calculates the Body Mass Index (BMI).
+
+weight = 70
+height = 1.70
+
+bmi = weight / height ** 2
+
+print(f"Weight: {weight} kg")
+print(f"Height: {height} m")
+print(f"BMI: {bmi}")
