931b1f4c233a45b8f8b92c485989b09cfd51228c
Alexia Almeida <alexia.karla@gmail.com>
2026-03-04 23:01:57 -0300

    average_grade.py
    This project calculates the arithmetic mean of a student's grades using basic Python variables and operations.

---
+++ average_grade.py
@@ -0,0 +1,10 @@
+english = 9
+history = 8
+chemistry = 6
+math = 4
+
+grades = [english, history, chemistry, math]
+
+average = sum(grades) / len(grades)
+
+print("The student's average grade is:", average)
